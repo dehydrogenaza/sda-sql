685417fca8761f5b55489213f977ab19e27ac8d9
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-01-16 20:34:57 +0100

    6.1 day 5 refactor main() method

--- src/main/java/movies_db/Client.java
+++ src/main/java/movies_db/Client.java
@@ -8,44 +8,43 @@ import movies_db.ui.*;
 import java.util.Arrays;
 
 public class Client {
+    private static final String CLI_MODE = "cli";
+    private static final String GUI_MODE = "gui";
+    private static final String MEMORY_STORAGE_MODE = "memory";
+    private static final String JDBC_STORAGE_MODE = "jdbc";
+    private static final String HIBERNATE_STORAGE_MODE = "hibernate";
+
     public static void main(String[] args) {
+        parseCommandlineArgs(args);
+
+        createMenu().loop();
+
+        StorageManager.close();
+    }
+
+    private static Menu createMenu() {
         Action add = new AddMovieAction(1);
         Action display = new DisplayMoviesAction(2);
         Action chooseStorage = new ChooseStorageAction(3);
         Action end = new EndProgramAction(4);
 
-//        if (args.length > 0) {
-//            UI.setUI(parseUICommandlineArg(args[0]));
-//        }
-        parseCommandlineArgs(args);
-
-        Menu mainMenu = new Menu(add, display, chooseStorage, end);
-        mainMenu.loop();
-        StorageManager.close();
+        return new Menu(add, display, chooseStorage, end);
     }
-
-//    private static IUserInterface parseUICommandlineArg(String arg) {
-//        return switch (arg.toUpperCase()) {
-//            case "CLI" -> new ConsoleUI();
-//            case "GUI" -> new SimpleGUI();
-//            default -> throw new RuntimeException("Niewłaściwy parametr UI w linii poleceń");
-//        };
-//    }
 
     private static void parseCommandlineArgs(String[] args) {
         if (args.length == 0) return;
 
-        if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase("CLI"))) {
+        if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase(CLI_MODE))) {
             UI.setUI(new ConsoleUI());
-        } else if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase("GUI"))) {
+        } else if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase(GUI_MODE))) {
             UI.setUI(new SimpleGUI());
         }
 
-        if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase("MEMORY"))) {
+        if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase(MEMORY_STORAGE_MODE))) {
             StorageManager.setInitialStorage(StorageType.IN_MEMORY);
-        } else if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase("JDBC"))) {
+        } else if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase(JDBC_STORAGE_MODE))) {
             StorageManager.setInitialStorage(StorageType.JDBC);
-        } else if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase("HIBERNATE"))) {
+        } else if (Arrays.stream(args).anyMatch( arg -> arg.equalsIgnoreCase(HIBERNATE_STORAGE_MODE))) {
             StorageManager.setInitialStorage(StorageType.HIBERNATE);
         }
     }
